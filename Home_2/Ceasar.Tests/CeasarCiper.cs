33c4bb07d4bd85f271c9a8e6c878186c007f9e32
krukovden <krukovden@gmail.com>
2015-10-07 16:43:21 +0300

    add home 2

---
+++ Home_2/Ceasar.Tests/CeasarCiper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ceasar.Tests
+{
+    public class CeasarCipher
+    {
+        public int offset { get; private set; }
+        public int maxValue { get; set; }
+        public int minValue { get; set; }
+
+        public CeasarCipher(int offset)
+        {
+            this.offset = offset;
+            maxValue = 126;
+            minValue = 33;
+        }
+
+        public string Encrypt(string message)
+        {
+            if (message == null)
+                throw new ArgumentNullException();
+
+            if (String.IsNullOrEmpty(message))
+                return message;
+
+            char[] mes = message.ToCharArray();
+
+            for (int i = 0; i < message.Length; i++)
+                if (mes[i] == 32) continue;
+                else
+                if (minValue <= mes[i] && mes[i] <= maxValue)
+                    mes[i] =
+                        (char)
+                            ((mes[i] + offset) <= maxValue
+                                ? ((mes[i] + offset) <= minValue ? maxValue : (mes[i] + offset))
+                                : ((mes[i] + offset) % maxValue + minValue - 1));
+                else
+                    throw new ArgumentOutOfRangeException(new char[] { mes[i] }.ToString());
+
+            return new string(mes);
+
+        }
+
+        public string Decrypt(string message)
+        {
+            offset *= -1;
+            string encoder = Encrypt(message);
+            offset *= -1;
+            return encoder;
+        }
+    }
+}
